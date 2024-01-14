bb1a593489faea419f25961b9b6244ba5a97df86
saiarun1856 <saiarunmahindra@gmail.com>
2024-01-14 23:49:35 +0530

    02basiccounter

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,33 @@
+import './App.css'
+import { useState } from 'react'
+function App() {
+  let [counter,setCounter] = useState(15)
+  const increase = ()=>{
+    if(counter <= 20){
+    counter = counter+1
+    setCounter(counter)
+    }
+  }
+  const decrease = ()=>{
+     if(counter>0){
+      counter = counter -1
+      setCounter(counter)
+     } 
+   } 
+    if(counter>0){
+      counter = counter - 1
+
+    }
+  return (
+    <>
+      <h1>chai aur code</h1>
+      <h2>counter value:{counter}</h2>
+
+      <button onClick={increase}>Add Value</button>
+      <br/>
+      <button onClick={decrease}>remove value</button>
+    </>
+  )
+}
+
+export default App
